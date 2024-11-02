f427ba37b13068e62fb1d7404781b3be9cef532f
Jason Bedard <jason+github@jbedard.ca>
2024-11-01 19:19:04 -0700

    test: update pnpm_lockfiles deps

--- e2e/pnpm_lockfiles/setup.sh
+++ e2e/pnpm_lockfiles/setup.sh
@@ -5,12 +5,16 @@
 #   6.1 - pnpm v8.6.0 bumped the lockfile version to 6.1
 #   9.0 - pnpm v9.0.0 bumped the lockfile version to 9.0; this includes breaking changes regarding lifecycle hooks and patches
 
+# Use an older node 18 before 'coreutils' and the package.json 'packageManager' interfere
+# the package manager version to ensure the version specified for npx is used.
+nvm use 18
+
 pushd base && npx pnpm@^7.0 install --lockfile-only && mv pnpm-lock.yaml ../v54/ && popd
 
 # pnpm v8.0.0 bumped the lockfile version to 6.0, 8.6.0 bumped it to 6.1 which was then reverted to 6.0
 # while still presenting minor differences from <8.6.0.
-pushd base && npx pnpm@8.5.1 install --lockfile-only && mv pnpm-lock.yaml ../v60/ && popd
-pushd base && npx pnpm@8.6.0 install --lockfile-only && mv pnpm-lock.yaml ../v61/ && popd
+pushd base && npx pnpm@8.5.1 install --lockfile-only && npx pnpm@8.5.1 upgrade --lockfile-only @types/sizzle && mv pnpm-lock.yaml ../v60/ && popd
+pushd base && npx pnpm@8.6.0 install --lockfile-only && npx pnpm@8.6.0 upgrade --lockfile-only @types/sizzle && mv pnpm-lock.yaml ../v61/ && popd
 
 # pnpm v9.0.0 bumped the lockfile version to 9.0
 pushd base && npx pnpm@^9.0 install --lockfile-only && mv pnpm-lock.yaml ../v90 && popd
